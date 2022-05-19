c6fddebdd9c319763557e4db0c46d27849370b92
Arseniy Simonov <catterer@yandex.ru>
2022-05-19 22:52:41 +0200

    suggestion constructor inheritance

--- 411/vtoroi.h
+++ 411/vtoroi.h
@@ -3,6 +3,8 @@
 #include "cl_base.h"
 class vtoroi : public cl_base {
 public:
-	vtoroi(std::string name, cl_base* p_predok = 0) : cl_base(name, p_predok) {};
+	// vtoroi(std::string name, cl_base* p_predok = 0) : cl_base(name, p_predok) {};
+	// вместо этого проще написать:
+	using cl_base::cl_base; // типа используй конструктор базового
 };
 #endif
